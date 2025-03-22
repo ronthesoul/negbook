b0a3be27d4afeef755e659a3b6931b441c7a6ae5
RonTheSoul <131078610+ronthesoul@users.noreply.github.com>
2025-03-22 19:22:26 +0200

    Update negbook.sh

--- negbook.sh
+++ negbook.sh
@@ -41,15 +41,21 @@ distro_check_and_install() {
 }
 
 
-function ask_user_packages(){
-package_list=$1
-echo "Enter package names one by one. Type 'exit' to finish:"
-while true; do
+ask_user_packages() {
+    local varname=$1
+    local pkg
+    local temp_list=()
+
+    echo "Enter package names one by one. Type 'exit' to finish:"
+    while true; do
         read -p "Package: " pkg
         if [[ "$pkg" == "exit" ]]; then
-        break
+            break
         elif [[ -n "$pkg" ]]; then
-        package_list+=("$pkg")
+            temp_list+=("$pkg")
         fi
     done
+
+    # Assign back to the original array by name
+    eval "$varname=(\"\${temp_list[@]}\")"
 }
