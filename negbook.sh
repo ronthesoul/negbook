485dca2928526ad4b3c75dddb83e94f55dd04b5a
RonTheSoul <131078610+ronthesoul@users.noreply.github.com>
2025-03-22 19:01:12 +0200

    Update negbook.sh

--- negbook.sh
+++ negbook.sh
@@ -2,13 +2,11 @@
 ###########################
 # Written by: Ron Negrov
 # Date: 3.21.2025
-# Purpose: A libray file that has usefull functions. 
+# Purpose: A library file that has useful functions.
 # Version: 0.0.12
 ###########################
-. /etc/os-release
 
-
-function install_missing_packages() {
+install_missing_packages() {
     local package_manager=$1
     shift
     local package_list=("$@")
@@ -22,18 +20,18 @@ function install_missing_packages() {
     echo "Finished installing packages"
 }
 
-function distro_check_and_install() {
+distro_check_and_install() {
     source /etc/os-release
 
     case "$ID" in
         debian|ubuntu)
-            install_missing_packages "sudo apt" "$@" 
+            install_missing_packages "sudo apt" "$@"
             ;;
         centos|rhel|fedora)
-            install_missing_packages "sudo yum" "$@" 
+            install_missing_packages "sudo yum" "$@"
             ;;
         arch)
-            install_missing_packages "sudo pacman -S --noconfirm" "$@" 
+            install_missing_packages "sudo pacman -S --noconfirm" "$@"
             ;;
         *)
             echo "Unsupported OS: $ID"
